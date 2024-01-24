39aca3bb51d8675879e78eac47b287a664af7fc5
Darren Jensen <darren.jensen@gmail.com>
2024-01-24 07:22:21 +0700

    update msg types to instruct serde to use snake case

--- src/msg.rs
+++ src/msg.rs
@@ -2,28 +2,33 @@ use cosmwasm_std::Addr;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+#[serde(rename_all = "snake_case")]
 pub struct GreetResp {
     pub message: String,
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+#[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
     Greet {},
     AdminsList {},
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+#[serde(rename_all = "snake_case")]
 pub struct AdminsListResp  {
     pub admins: Vec<Addr>,
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+#[serde(rename_all = "snake_case")]
 pub struct InstantiateMsg {
     pub admins: Vec<String>,
     pub donation_denom: String,
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+#[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
     AddMembers { admins: Vec<String> },
     Leave {},
